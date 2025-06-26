4f13f342f77dcd8a3a407d68498258119fb4e16b
Mohammed Ziauddin <52736506+zia-aws@users.noreply.github.com>
2025-06-27 02:56:50 +0530

    Update settings.py

--- Auction/settings.py
+++ Auction/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'bid_app/static')]  # Where your venobox.js is
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where Django collects them for deployment
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
